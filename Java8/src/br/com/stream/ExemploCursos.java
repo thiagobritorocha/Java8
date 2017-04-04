47115dc4500e1d594a5b3ed88de25fa2461b7681
Thiago Brito Rocha <thiagobritorocha@yahoo.com.br>
2017-04-03 21:14:58 -0300

    MapToInt

--- Java8/src/br/com/stream/ExemploCursos.java
+++ Java8/src/br/com/stream/ExemploCursos.java
@@ -4,26 +4,29 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-class Curso{	
-	
+class Curso {
+
 	public Curso(String nome, Integer alunos) {
 		super();
 		this.nome = nome;
 		this.alunos = alunos;
 	}
-	
+
 	String nome;
 	Integer alunos;
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Integer getAlunos() {
 		return alunos;
 	}
+
 	public void setAlunos(Integer alunos) {
 		this.alunos = alunos;
 	}
@@ -37,11 +40,12 @@ public class ExemploCursos {
 		cursos.add(new Curso("JavaScript", 150));
 		cursos.add(new Curso("Java8", 112));
 		cursos.add(new Curso("C", 55));
-		
+
 		cursos.sort(Comparator.comparing(Curso::getAlunos));
 		cursos.forEach(c -> System.out.println(c.getNome()));
-		cursos.stream().filter(c -> c.getAlunos() > 100).
-			map(Curso::getAlunos).
-			forEach(System.out::println);
+		cursos.stream().filter(c -> c.getAlunos() > 100).map(Curso::getAlunos).forEach(System.out::println);
+		
+		int sum = cursos.stream().filter(c -> c.getAlunos() > 100).mapToInt(Curso::getAlunos).sum();
+		System.out.println(sum);
 	}
 }
